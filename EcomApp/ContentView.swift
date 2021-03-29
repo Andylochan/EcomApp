93c251be5aa2c0319bb7096732ee872fbaab841e
Andy Lochan <36180382+Andylochan@users.noreply.github.com>
2021-03-29 19:27:59 -0400

    Initial Commit

---
+++ EcomApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  EcomApp
+//
+//  Created by Andy Lochan on 3/29/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ EcomApp/EcomAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcomAppApp.swift
+//  EcomApp
+//
+//  Created by Andy Lochan on 3/29/21.
+//
+
+import SwiftUI
+
+@main
+struct EcomAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
