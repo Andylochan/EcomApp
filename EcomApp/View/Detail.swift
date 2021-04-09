66e1a05315f9a88036c708d1e6f2af4ca7858f2a
Andy Lochan <36180382+Andylochan@users.noreply.github.com>
2021-04-09 00:33:10 -0400

    Add ColorPicker, Buttons, ScrollView

--- EcomApp/View/Detail.swift
+++ EcomApp/View/Detail.swift
@@ -12,89 +12,180 @@ struct Detail: View {
     @Binding var show: Bool
     var animation: Namespace.ID
     
+    @State private var loadContent = false
+    @State private var selectedColor: Color = Color("p1")
+    
     var body: some View {
         
-        VStack {
+        // optimization for smaller screen sizes
+        ScrollView(UIScreen.main.bounds.height < 850 ? .vertical : .init(), content: {
             
-            HStack(spacing: 25) {
+            VStack {
                 
-                Button(action: {
-                    // closing view...
-                    withAnimation(.spring()){show.toggle()}
-                }) {
-                    Image(systemName: "chevron.left")
-                        .font(.title)
-                        .foregroundColor(.black)
-                }
-                
-                Spacer()
-                
-                Button(action: {}) {
-                    Image(systemName: "magnifyingglass")
-                        .font(.title)
-                        .foregroundColor(.black)
-                }
-                
-                Button(action: {}) {
-                    Image(systemName: "bag")
-                        .font(.title)
-                        .foregroundColor(.black)
-                }
-                
-            }
-            .padding()
-            
-            VStack(spacing: 10) {
-                
-                Image(selectedItem.image)
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(height: 240)
-                    // since id is common...
-                    .matchedGeometryEffect(id: "image\(selectedItem.id)", in: animation)
-                    .padding()
-                
-                Text(selectedItem.title)
-                    .font(.title)
-                    .fontWeight(.heavy)
-                    .foregroundColor(.black)
-                
-                Text(selectedItem.subTitle)
-                    .foregroundColor(.gray)
-                    .padding(.top, 4)
-                
-                HStack {
+                HStack(spacing: 25) {
                     
-                    Text(selectedItem.rating)
-                        .fontWeight(.heavy)
-                        .foregroundColor(.black)
-                        .matchedGeometryEffect(id: "rating\(selectedItem.id)", in: animation)
+                    Button(action: {
+                        // closing view...
+                        withAnimation(.spring()){show.toggle()}
+                    }) {
+                        Image(systemName: "chevron.left")
+                            .font(.title)
+                            .foregroundColor(.black)
+                    }
                     
                     Spacer()
                     
                     Button(action: {}) {
-                        
-                        Image(systemName: "suit.heart")
-                            .font(.title2)
+                        Image(systemName: "magnifyingglass")
+                            .font(.title)
                             .foregroundColor(.black)
                     }
-                    .matchedGeometryEffect(id: "heart\(selectedItem.id)", in: animation)
+                    
+                    Button(action: {}) {
+                        Image(systemName: "bag")
+                            .font(.title)
+                            .foregroundColor(.black)
+                    }
+                    
                 }
                 .padding()
+                
+                VStack(spacing: 10) {
+                    
+                    Image(selectedItem.image)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(height: 240)
+                        // since id is common...
+                        .matchedGeometryEffect(id: "image\(selectedItem.id)", in: animation)
+                        .padding()
+                    
+                    Text(selectedItem.title)
+                        .font(.title)
+                        .fontWeight(.heavy)
+                        .foregroundColor(.black)
+                    
+                    Text(selectedItem.subTitle)
+                        .foregroundColor(.gray)
+                        .padding(.top, 4)
+                    
+                    HStack {
+                        
+                        Text(selectedItem.rating)
+                            .fontWeight(.heavy)
+                            .foregroundColor(.black)
+                            .matchedGeometryEffect(id: "rating\(selectedItem.id)", in: animation)
+                        
+                        Spacer()
+                        
+                        Button(action: {}) {
+                            
+                            Image(systemName: "suit.heart")
+                                .font(.title2)
+                                .foregroundColor(.black)
+                        }
+                        .matchedGeometryEffect(id: "heart\(selectedItem.id)", in: animation)
+                    }
+                    .padding()
+                }
+                .padding(.top, 35)
+                .background(
+                    Color(selectedItem.image)
+                        .clipShape(CustomShape())
+                        .matchedGeometryEffect(id: "color\(selectedItem.id)", in: animation)
+                )
+                .cornerRadius(15)
+                .padding()
+                
+                // delay loading the content for smooth animation...
+                VStack {
+                    
+                    VStack(alignment: .leading, spacing: 8) {
+                        
+                        Text("Exclusive Offer")
+                            .fontWeight(.heavy)
+                            .foregroundColor(.black)
+                            
+                        Text("Financing as low as 0.9% APR")
+                            .foregroundColor(.secondary)
+                    }
+                    .padding()
+                    .frame(width: UIScreen.main.bounds.width - 30, alignment: .leading)
+                    .background(Color("p4"))
+                    .cornerRadius(15)
+                    
+                    VStack(alignment: .leading, spacing: 10) {
+                        
+                        Text("Color")
+                            .fontWeight(.heavy)
+                            .foregroundColor(.black)
+                        
+                        HStack(spacing: 15) {
+                            
+                            ForEach(1...4, id: \.self) { i in
+                                
+                                ZStack {
+                                    
+                                    Color("p\(i)")
+                                        .clipShape(Circle())
+                                        .frame(width: 45, height: 45)
+                                        .onTapGesture {
+                                            withAnimation {
+                                                selectedColor = Color("p\(i)")
+                                            }
+                                        }
+                                    
+                                    // checkmark for selected circle...
+                                    
+                                    if selectedColor == Color("p\(i)") {
+                                        Image(systemName: "checkmark")
+                                            .foregroundColor(.black)
+                                    }
+                                }
+                            }
+                            
+                            Spacer(minLength: 0)
+                        }
+                    }
+                    .padding()
+                    
+                    Spacer(minLength: 15)
+                    
+                    Button(action: {}) {
+                        Text("CONTACT US")
+                            .fontWeight(.bold)
+                            .foregroundColor(.black)
+                            .padding(.vertical)
+                            .frame(width: UIScreen.main.bounds.width - 100)
+                            .background(
+                                RoundedRectangle(cornerRadius: 15)
+                                    .stroke(Color.black, lineWidth: 1)
+                            )
+                    }
+                    
+                    Button(action: {}) {
+                        Text("ADD TO CART")
+                            .fontWeight(.bold)
+                            .foregroundColor(.white)
+                            .padding(.vertical)
+                            .frame(width: UIScreen.main.bounds.width - 100)
+                            .background(Color.black)
+                            .cornerRadius(15)
+                    }
+                    .padding(.vertical)
+                }
+                .padding([.horizontal, .bottom])
+                .frame(height: loadContent ? nil : 0)
+                .opacity(loadContent ? 1 : 0)
+                // for smooth transition...
+                
+                Spacer(minLength: 0)
             }
-            .padding(.top, 35)
-            .background(
-                Color(selectedItem.image)
-                    .clipShape(CustomShape())
-                    .matchedGeometryEffect(id: "color\(selectedItem.id)", in: animation)
-            )
-            .cornerRadius(15)
-            .padding()
-            
-            // delay 
-            
-            Spacer()
+        })
+        .onAppear {
+            withAnimation(Animation.spring().delay(0.45)) {
+                loadContent.toggle()
+            }
         }
     }
 }
-
